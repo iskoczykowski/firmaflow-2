a485b8641104a4d20a71ca2400840528bd768ab7
iskoczykowski <i.skoczykowski@gmail.com>
2026-07-01 22:51:06 +0200

    Replace DashboardScreen with AppNavigator

--- App.tsx
+++ App.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import DashboardScreen from './src/screens/Dashboard/DashboardScreen';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#F4F7FB' }}>
-      <StatusBar style="dark" />
-      <DashboardScreen />
-    </SafeAreaView>
-  );
+  return <AppNavigator />;
 }
