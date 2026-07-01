3b19f5cdbc255f68bb03dbbb7bea913d41e432d3
iskoczykowski <i.skoczykowski@gmail.com>
2026-07-01 23:50:13 +0200

    Update AppCard.tsx

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -1,1 +1,33 @@
+import React from "react";
+import { TouchableOpacity, View, ViewStyle } from "react-native";
+import { cardStyle } from "../theme/theme";
 
+type Props = {
+  children: React.ReactNode;
+  onPress?: () => void;
+  style?: ViewStyle;
+};
+
+export default function AppCard({
+  children,
+  onPress,
+  style,
+}: Props) {
+  if (onPress) {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.9}
+        onPress={onPress}
+        style={[cardStyle, style]}
+      >
+        {children}
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <View style={[cardStyle, style]}>
+      {children}
+    </View>
+  );
+}
